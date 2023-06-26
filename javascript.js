d9467b94a43af8bb8d077b7874ee4084a2a4c62f
nvtinh <nvtinh368@gmail.com>
2023-06-26 13:01:35 +0700

    Update dashboard

--- javascript.js
+++ javascript.js
@@ -1,12 +1,21 @@
+let toggle = document.querySelector(".mobile_btn");
+let mainBody = document.querySelector(".main-body");
+let navigation = document.querySelector(".navigation");
+
+toggle.addEventListener("click", function() {
+  mainBody.classList.toggle('active');
+  navigation.classList.toggle('active');
+});
+
+
 $(window).scroll(function(){
   var sticky = $('.sticky-top nav'),
       scroll = $(window).scrollTop();
 
   if (scroll >= 50){
   	sticky.addClass('scrolled');
-  	console.log("add");
   }else{
-  	console.log("rev");
   	sticky.removeClass('scrolled');
   } 
 });
+
