fa524d29f92c5acb297db05e818b09e90c355884
nvtinh368 <nvtinh368@gmail.com>
2023-06-27 14:45:36 +0700

    them paging

--- javascript.js
+++ javascript.js
@@ -8,6 +8,54 @@ toggle.addEventListener("click", function() {
 });
 
 
+// khi trang được load thì thực hiện lấy tổng số trang ra
+// Sau đó thực hiện hiện load trang và nạp vào javascript
+
+async function loadOrder(page){
+	// $("#content").html('')
+	$.ajax({
+		url: 'https://dummyjson.com/products/1'
+	})
+	.then(rs =>{
+		var arr = new Array(rs['stock']);
+		$('#paging').pagination({
+			dataSource: arr,
+	   		 pageSize:40,
+	    	afterPageOnClick : function(event,page){
+	    		loadPage(page);
+	   	 	}
+		})
+	})
+	.catch(err=>{
+		console.log("không load được dữ liệu");
+	})
+
+	
+}
+
+// Thực hiện load và làm mới lại trang
+function loadPage(page){
+	$("#content").html('')
+	$.ajax({
+		url: 'user?page='+page
+	})
+	.then(rs =>{
+		for (let i =0; i < rs.data.lenght; i++){
+			const element = rs.data[i];
+			var item = $(`<h3>${element.user}</h3>`);
+			$("#content").append(item)
+		}
+	})
+	.catch(err=>{
+		console.log("không load được dữ liệu");
+	})
+	
+}
+
+
+
+
+
 $(window).scroll(function(){
   var sticky = $('.sticky-top nav'),
       scroll = $(window).scrollTop();
